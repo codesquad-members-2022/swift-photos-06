754cda2db8e5c2b4adb5f43eeee6c8292e188607
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-03-21 11:25:25 +0900

    [CREATE]Project tests

---
+++ group6/group6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  group6
+//
+//  Created by Kai Kim on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
