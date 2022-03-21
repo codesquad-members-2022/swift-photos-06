1506074767552d7345e2c81bba7e436e409c202b
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-03-21 17:38:22 +0900

    [ADD]Shape & Color XCTest

--- group6/group6Tests/ShapeTests.swift
+++ group6/group6Tests/ShapeTests.swift
@@ -10,17 +10,40 @@ import XCTest
 
 class ShapeTests: XCTestCase {
 
-    var model : Shape!
-    
     override func setUpWithError() throws {
-        model = Shape(id: Id() ,color:Color(), size: Size(width : 80, height : 80))
     }
 
     override func tearDownWithError() throws {
+        
     }
 
     func testChangeColor() throws {
         
+        //Color 생성
+        var r : Double = 0
+        var g : Double = 19
+        var b : Double = 3
+        
+        var colorTest1 = Color(r: r, g: g, b: b)
+         
+        
+        //Color errors
+        var colorTest2 = Color(r: -1, g: 292, b: 1) //0~255 범위 초과
+        colorTest1.blue = 50 //속성값 private let 으로 설정
+        colorTest1.red = 120
+        colorTest1.green = 122
+
+        
+        //속성값들 은 Private let 으로 설정, 외부에서 접근 불가.
+        var model = Shape(id: Id(),color: Color(r: 1, g: 2, b: 3), size: Size(width:80 , height:80))
+        
+        
+        //변수 변경 불가.
+        model.color = Color.white
+        model.size = Size(80,80)
+        model.id = "#32152"
+    
+  
     }
     
     func testPerformanceExample() throws {
