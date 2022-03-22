3e205faf1124bd0b6171612f7c24bcf774886cfc
wnsxor1993 <wnsxor1993@naver.com>
2022-03-22 14:10:03 +0900

    Feat: CollectionView 기본 기능 구현

---
+++ group6/group6/View/CustomCollectionView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionView.swift
+//  group6
+//
+//  Created by juntaek.oh on 2022/03/22.
+//
+
+import Foundation
+import UIKit
+
+class CustomCollectionView: UICollectionView{
+    
+}
---
+++ group6/group6/View/RectangleCollectionCell.swift
@@ -0,0 +1,19 @@
+//
+//  RectangleCollectionCell.swift
+//  group6
+//
+//  Created by juntaek.oh on 2022/03/22.
+//
+
+import Foundation
+import UIKit
+
+class RectangleCollectionCell: UICollectionViewCell{
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+        
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
